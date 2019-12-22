5904fd0bb44640c3e194b59924e6f2560d40ea10
Mikaeel <ghmikaeel@gmail.com>
2019-12-22 14:57:12 +0330

    Menus added on main

--- Plants vs Zombies/src/Main.java
+++ Plants vs Zombies/src/Main.java
@@ -7,6 +7,14 @@ public class Main {
     public static Menu loginMenu;
     public static Menu mainMenu;
     public static Menu leaderboard;
+    public static Menu profileMenu;
+    public static Menu playMenu;
+    public static Menu collectionMenu;
+    public static Menu shopMenu;
+    public static Menu dayAndWaterMenu;
+    public static Menu railMenu;
+    public static Menu zombieMenu;
+    public static Menu pvpMenu;
 
     public static String scanLine() {
         return scanner.nextLine();
