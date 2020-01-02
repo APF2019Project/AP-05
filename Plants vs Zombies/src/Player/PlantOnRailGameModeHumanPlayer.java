17738ddab92fa6e02ef867663e0c293db0bf350e
arshia <arshia.soltani2@gmail.com>
2020-01-02 23:43:44 +0330

    write plantRailPlayer

--- Plants vs Zombies/src/Player/PlantOnRailGameModeHumanPlayer.java
+++ Plants vs Zombies/src/Player/PlantOnRailGameModeHumanPlayer.java
@@ -5,11 +5,13 @@ import Command.PlantOnDayAndWaterModePlayerCommandHandler;
 import Command.PlantOnRailModePlayerCommandHandler;
 import Main.*;
 
+import java.util.Random;
+
 public class PlantOnRailGameModeHumanPlayer extends PlantPlayer {
     public PlantOnRailGameModeHumanPlayer(User user) {
         super(user);
     }
-
+    private int remainTurnToAddCard=0;
     @Override
     public void doAction() throws Exception {
         Menu PlantOnRailModePlayerMenu = new Menu(user, new PlantOnRailModePlayerCommandHandler());
@@ -18,7 +20,16 @@ public class PlantOnRailGameModeHumanPlayer extends PlantPlayer {
 
     @Override
     public void gameAction() {
-
+        Random random=new Random();
+        if(remainTurnToAddCard==0){
+            if(this.getCreaturesOnHand().size()!=10) {
+                remainTurnToAddCard = 1 + random.nextInt(3);
+                Plant plant = Plant.getAllPlants().get(random.nextInt(Plant.getAllPlants().size()));
+                this.addCreaturesOnHand(plant);
+            }
+        }else{
+            remainTurnToAddCard--;
+        }
     }
 
     @Override
