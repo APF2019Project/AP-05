9690d7d46f49b05ee78be13467a24f8b74685091
Mikaeel <ghmikaeel@gmail.com>
2020-01-29 22:33:10 +0330

    zombie doaction bug fixed

--- Plants vs Zombies/src/Main/GameMenuSwitcher.java
+++ Plants vs Zombies/src/Main/GameMenuSwitcher.java
@@ -21,50 +21,56 @@ public class GameMenuSwitcher {
     private void runTurn() throws Exception {
         System.out.println("your turn Started");
         map.getPlantPlayer().doAction(() -> {
-            try {
-                System.out.println("your turn ended");
-                if (gameStatus.equals(GameStatus.OnGame)) {
-                    map.getZombiePlayer().doAction(null);
-                }
-                if (gameStatus.equals(GameStatus.OnGame)) {
-                    gameStatus = map.run();
-                }
-                if (gameStatus.equals(GameStatus.OnGame)) {
-                    map.getPlantPlayer().gameAction();
-                }
-                if (gameStatus.equals(GameStatus.OnGame)) {
-                    map.getZombiePlayer().gameAction();
-                }
-                if (gameStatus.equals(GameStatus.OnGame)) {
-                    runTurn();
-                } else {
-                    if (!gameStatus.equals(GameStatus.notInGame)) {
-                        JSONObject data = new JSONObject();
-                        data.put("message", GameData.winMessage);
-                        data.put("messageType", "INFORMATION");
-                        (gameStatus.equals(GameStatus.PlantPlayerWins) ? map.getPlantPlayer() : map.getZombiePlayer())
-                                .getConnection().send("showMessage", data);
+            System.out.println("your turn ended");
+            if (gameStatus.equals(GameStatus.OnGame)) {
+                try {
+                    map.getZombiePlayer().doAction(() -> {
+                        try {
+                            if (gameStatus.equals(GameStatus.OnGame)) {
+                                gameStatus = map.run();
+                            }
+                            if (gameStatus.equals(GameStatus.OnGame)) {
+                                map.getPlantPlayer().gameAction();
+                            }
+                            if (gameStatus.equals(GameStatus.OnGame)) {
+                                map.getZombiePlayer().gameAction();
+                            }
+                            if (gameStatus.equals(GameStatus.OnGame)) {
+                                runTurn();
+                            } else {
+                                if (!gameStatus.equals(GameStatus.notInGame)) {
+                                    JSONObject data = new JSONObject();
+                                    data.put("message", GameData.winMessage);
+                                    data.put("messageType", "INFORMATION");
+                                    (gameStatus.equals(GameStatus.PlantPlayerWins) ? map.getPlantPlayer() : map.getZombiePlayer())
+                                            .getConnection().send("showMessage", data);
 
-                        data = new JSONObject();
-                        data.put("message", GameData.loseMessage);
-                        data.put("messageType", "INFORMATION");
-                        (gameStatus.equals(GameStatus.ZombiePlayerWins) ? map.getPlantPlayer() : map.getZombiePlayer())
-                                .getConnection().send("showMessage", data);
+                                    data = new JSONObject();
+                                    data.put("message", GameData.loseMessage);
+                                    data.put("messageType", "INFORMATION");
+                                    (gameStatus.equals(GameStatus.ZombiePlayerWins) ? map.getPlantPlayer() : map.getZombiePlayer())
+                                            .getConnection().send("showMessage", data);
 
-                        map.getPlantPlayer().getConnection().popMenu();
-                        map.getPlantPlayer().getConnection().popMenu();
+                                    map.getPlantPlayer().getConnection().popMenu();
+                                    map.getPlantPlayer().getConnection().popMenu();
 
-                        map.getZombiePlayer().getConnection().popMenu();
-                        map.getZombiePlayer().getConnection().popMenu();
+                                    map.getZombiePlayer().getConnection().popMenu();
+                                    map.getZombiePlayer().getConnection().popMenu();
 
-                        map.getPlantPlayer().getConnection().getUser().gameEnded(gameStatus.equals(GameStatus.PlantPlayerWins));
-                        map.getZombiePlayer().getConnection().getUser().gameEnded(gameStatus.equals(GameStatus.ZombiePlayerWins));
-                        Main.print(gameStatus.name());
-                    }
-                    setGameStatus(GameStatus.notInGame);
+                                    map.getPlantPlayer().getConnection().getUser().gameEnded(gameStatus.equals(GameStatus.PlantPlayerWins));
+                                    map.getZombiePlayer().getConnection().getUser().gameEnded(gameStatus.equals(GameStatus.ZombiePlayerWins));
+                                    Main.print(gameStatus.name());
+                                }
+                                setGameStatus(GameStatus.notInGame);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
             return null;
         });
