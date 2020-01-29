fba319813477763401e7841f103d91abc9ccf964
Mikaeel <ghmikaeel@gmail.com>
2020-01-29 23:32:00 +0330

    show lawn method bug fixed

--- Plants vs Zombies/src/Command/ZombiePlayerCommandHandler.java
+++ Plants vs Zombies/src/Command/ZombiePlayerCommandHandler.java
@@ -4,6 +4,7 @@ import Main.ActiveCard;
 import Main.GameData;
 import Main.Main;
 import Objects.Creature;
+import Objects.GunShot;
 import Objects.Zombie;
 import Player.ZombiePlayer;
 import org.json.simple.JSONArray;
@@ -83,15 +84,27 @@ public class ZombiePlayerCommandHandler extends CommandHandler {
         menu.exit();
     }
 
-
     void showLawn(InputCommand inputCommand) throws Exception {
         JSONArray jsonArray = new JSONArray();
         for (ActiveCard activeCard : menu.getConnection().getUser().getPlayer().getMap().getActiveCardArrayList()) {
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("activeCard.getCreature.getName", activeCard.getCreature().getName());
-            jsonObject.put("activeCard.getX", activeCard.getX());
-            jsonObject.put("activeCard.getY", activeCard.getY());
-            jsonObject.put("activeCard.getRemainingHp", activeCard.getRemainingHp());
+            jsonObject.put("name", activeCard.getCreature().getName());
+            jsonObject.put("type", (activeCard.getCreature() instanceof Zombie) ? "Zombie" : "Plant");
+            jsonObject.put("x", activeCard.getX());
+            jsonObject.put("y", activeCard.getY());
+            jsonObject.put("remaining hp", activeCard.getRemainingHp());
+            if (activeCard.getCreature() instanceof Zombie) {
+                jsonObject.put("speed", ((Zombie) activeCard.getCreature()).getSpeed());
+            }
+            jsonArray.add(jsonObject);
+        }
+        for (GunShot gunShot : menu.getConnection().getUser().getPlayer().getMap().getGunShotArrayList()) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", gunShot.getGun().getName());
+            jsonObject.put("type", "GunShot");
+            jsonObject.put("x", gunShot.getX());
+            jsonObject.put("y", gunShot.getY());
+            jsonObject.put("speed", gunShot.getVx());
             jsonArray.add(jsonObject);
         }
         menu.getConnection().send("showLawn", jsonArray);
